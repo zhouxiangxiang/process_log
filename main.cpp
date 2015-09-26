8669b4ec63aaf64d3a8a4a31df495334284f7af0
zhouxiangxiang <zhxiangxiang@gmail.com>
2015-09-26 14:56:45 +0800

    1.4 Laying Out Widgets

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
 #include <QApplication>
-#include <QLabel>
-#include <QPushButton>
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QSpinBox>
+#include <QSlider>
+
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
-    QLabel lab("<h2><i> Hello </i>"
-               "<font color=red> Qt </font> </h2>"
-               );
-    lab.setFixedSize(500, 300);
-    //lab.setGeometry(100, 100, 1000, 500);
-    lab.show();
+    QWidget *pwin = new QWidget;
+    pwin->setWindowTitle("ENTER YOU AGE");
 
-    QPushButton* pbtn = new QPushButton;
-    pbtn->setText("quit");
-    pbtn->setParent(&lab);
-    QObject::connect(pbtn, SIGNAL(clicked()), &app, SLOT(quit()));
-    pbtn->show();
+    QSpinBox *pSpinbox = new QSpinBox;
+    QSlider  *pSlider = new QSlider(Qt::Horizontal);
+    pSpinbox->setRange(0, 130);
+    pSlider->setRange(0, 130);
+    pSpinbox->setValue(35);
 
+    QObject::connect(pSpinbox, SIGNAL(valueChanged(int)), pSlider, SLOT(setValue(int)));
+    QObject::connect(pSlider, SIGNAL(valueChanged(int)), pSpinbox, SLOT(setValue(int)));
+
+    QHBoxLayout *playout = new QHBoxLayout;
+    playout->addWidget(pSpinbox);
+    playout->addWidget(pSlider);
+    pwin->setLayout(playout);
+
+    pwin->show();
     return app.exec();
 }
